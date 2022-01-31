1c5d461c51f2d72e12810b39f9c6c1eb96661128
Travis Houston <traviswaynehouston@gmail.com>
2022-01-30 17:59:01 -0700

    converted all info in fetch into real dollar amounts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,13 @@
 //       console.log(e)
 //     })
 // ----------------------------------------------------------------------------------------------------
-
+let num = 6.32434234
+let n = num.toFixed(2)
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+})
+console.log(n)
 // fetch request getting news story and posting card elem
 fetch('https://data.messari.io/api/v1/news')
     .then(response => response.json())
@@ -43,24 +49,45 @@ fetch('https://data.messari.io/api/v1/assets')
     .then(response => response.json())
     .then(assets => {
       for (i = 0;i <= 19; i++ ) {
-        let currencyList = document.createElement('div')
+        let price = assets.data[i].metrics.market_data.price_usd
+        let newPrice = formatter.format(price)
+        let lowPrice = assets.data[i].metrics.cycle_low.price
+        let newLowPrice = formatter.format(lowPrice)
+        let highPrice = assets.data[i].metrics.all_time_high.price
+        let newHighPrice = formatter.format(highPrice)
+        let MCAP = assets.data[i].metrics.marketcap.current_marketcap_usd
+        let MCAPRounded = Math.round(MCAP)
+        let MCAPString = MCAPRounded.toString()
+        let newMCAP = MCAPString.slice(0, 3) + 'B'
+        let tFH = assets.data[i].metrics.market_data.percent_change_usd_last_24_hours
+        let tFHString = tFH.toString()
+        let newTFH = tFHString.slice(0, 5) + '%'
+
+        const currencyList = document.createElement('div')
         currencyList.innerHTML = `
         <ul class="collection">
         <li class="collection-item avatar">
         <img src="" alt="" class="circle">
         <span class="title">${assets.data[i].name}</span>
-        <p>Price:$ ${assets.data[i].metrics.market_data.price_usd} <br>
-           All time high:$${assets.data[i].metrics.all_time_high.price} <br>
-           Cycle Low:$${assets.data[i].metrics.cycle_low.price} <br>
-           MCAP:$${assets.data[i].metrics.marketcap.current_marketcap_usd} 24H:${assets.data[i].metrics.market_data.percent_change_usd_last_24_hours}
+        <p>Price:$ ${newPrice} <br>
+           All time high:${newHighPrice} <br>
+           Cycle Low:${newLowPrice} <br>
+           MCAP:${newMCAP} 24H:${newTFH}
         </p>
         <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
       </li>
       </ul>
         `
         document.getElementById('currency').append(currencyList)
+
+
+        console.log(newLowPrice)
+        console.log(newHighPrice)
+        console.log(newMCAP)
+        console.log(newTFH)
+        console.log(newPrice)
+        // console.log(assets.data[i].metrics.cycle_low.price)
       }
-        console.log(assets.data)
     })
 
     // name: ${assets.data[0].name}
@@ -68,3 +95,4 @@ fetch('https://data.messari.io/api/v1/assets')
     // Price:$ ${assets.data[0].metrics.market_data.price_usd}
     // All time high:$ ${assets.data[0].metrics.all_time_high.price}
     // Cycle low:$ ${assets.data[0].metrics.cycle_low.price}
+
